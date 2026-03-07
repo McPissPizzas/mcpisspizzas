235c3fadccec73a7953ba964dc917317a02392d6
McPissPizzas <bluejw444@gmail.com>
2026-03-07 14:55:14 +0100

    Refactor pizza creation and audio handling logic

--- MPP.js
+++ MPP.js
@@ -12,20 +12,6 @@ for (i = 0; i < 70; i++) {
     bild.src = "pizza_pfeil.png"
     pizza.appendChild(bild)
    
-const register = document.getElementById("Kopfleiste")
-const spalten = 5.5;
-const reihen = 5.5;
-
-for (i = 0; i < 70; i++) {
-    const x = (i % spalten) * (150 / spalten)
-    const y = Math.floor(i / spalten ) * (100 / reihen)
-
-    const pizza = document.createElement("div")
-    const bild = document.createElement("img")
-
-    bild.src = "pizza_pfeil.png"
-    pizza.appendChild(bild)
-   
 
     pizza.classList = "Pizza"
     document.body.appendChild(pizza)
@@ -79,19 +65,3 @@ btn.addEventListener("click", () => {
 //     })
 
 // })
-
-    pizza.classList = "Pizza"
-    document.body.appendChild(pizza)
-
-    
-
-    pizza.style.left = x -20  + "vw"
-    pizza.style.top = y -100 + "vh"
-
-
-}
-function Animpress(){
-    presses = 0
-    presses +=1
-}
-
